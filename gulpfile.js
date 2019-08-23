2fda348778492f067a4be4ca880faa4247c2631b
Miguel Ángel Pérez Martínez <miguelangelperezmartinez@not.a.mail>
2019-08-23 14:53:32 -0300

    watcher for shaders

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,15 @@ function javascript(cb) {
     });
 }
 
+function shaders(cb) {
+    exec('./build-scripts/minify_glsl.sh src/shaders src/shaders.js', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+}
+
 exports.default = () => {
     watch('src/**/*.js', javascript);
+    watch(['src/shaders/**/*.vert','src/shaders/**/*.frag'],shaders);
 }
